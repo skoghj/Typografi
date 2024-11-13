53ca357137469561c0f9397eab798029528a1c71
Huijin <huba0001@stud.kea.dk>
2024-11-13 16:45:42 +0100

    breakpoint tilføjet

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,6 +9,13 @@ export default {
         alata: ['"Alata"', "sans-serif"],
         schibsted: ["Schibsted-Grotesk", "sans-serif"],
       },
+      screens: {
+        "sm-custom": "546px",
+        "md-custom": "768px",
+        "lg-custom": "1024px",
+        "xl-custom": "1440px",
+        "xxl-custom": "2560px",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
